e59baaa2b5d97ce65c1425f3cba4aca494e0558e
math77 <matheusemanuel745@gmail.com>
2018-04-28 00:51:37 -0300

    Alterações iniciais

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='MapaDaGentileza',
       version='1.0',
       description='OpenShift App',
-      author='Mateus',
-      author_email='example@example.com',
+      author='Matheus Emanuel, Pedro Baesse',
+      author_email='matheusemanuel@gmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe', 'flask-sqlalchemy','sqlalchemy-migrate','flask-whooshalchemy'],
+      install_requires=['Flask>=1.0.0', 'MarkupSafe', 'flask-sqlalchemy','sqlalchemy-migrate','flask-whooshalchemy'],
       )
