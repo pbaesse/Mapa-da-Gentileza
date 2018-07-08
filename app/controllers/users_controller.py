164e0cb330a4ffa3f2a3671f000296a7def9c317
math77 <matheusemanuel745@gmail.com>
2018-07-08 19:43:37 -0300

    Dois novos campos em Users model

---
+++ app/controllers/users_controller.py
@@ -0,0 +1,33 @@
+from app.models import Users
+
+
+class UsersController:
+
+
+	def save_new_user(self, user):
+		pass
+
+
+	def update_profile(self, user):
+		pass
+
+
+	def get_all_users(self):
+		pass
+
+
+	def get_user_by_id(self, user):
+		pass
+
+
+	def generate_unique_username(self, user):
+		pass
+
+
+	def delete_account(self, user):
+		pass
+
+
+	def update_password(self, user):
+		pass
+		
--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class Users(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	username = db.Column(db.String(30), unique=True, nullable=False)
+	first_name = db.Column(db.String(50), nullable=False)
+	last_name = db.Column(db.String(50))
 	email = db.Column(db.String(130), unique=True, nullable=False)
 	about_me = db.Column(db.Text)
 	password_hash = db.Column(db.String(200), nullable=False)
