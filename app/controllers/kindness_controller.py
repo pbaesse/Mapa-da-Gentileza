0131675654d6e9260cb24e6860a5eac282a5c340
math77 <matheusemanuel745@gmail.com>
2018-07-07 21:42:20 -0300

    Controllers definidos

---
+++ app/controllers/kindness_controller.py
@@ -0,0 +1,28 @@
+from app.models import Kindness
+
+
+class KindnessController:
+
+
+	def save_new_kindness(self, kindness):
+		pass
+
+
+	def delete_kindness(self, kindness):
+		pass
+
+
+	def update_kindness(self, kindness):
+		pass
+
+
+	def save_new_kindness_anonymity(self, kindness):
+		pass
+
+
+	def get_all_kindness(self):
+		pass
+
+
+	def get_kindness_by_id(self, kindness):
+		pass
