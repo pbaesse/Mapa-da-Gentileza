d69db646f7221cc4a7bdda9937916994d1c06376
Matheus Emanuel <matheusemanuel745@gmail.com>
2019-02-05 00:20:30 -0300

    Substituido o framework frontend uikit para a utilização do semantic ui

--- app/static/js/profile.js
+++ app/static/js/profile.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
   updateProfile();
+  getProfile();
   $("#edit-profile").click(function(){
-    //chamara a função getProfile() e popular o form do modal
-    // para usuários atualizarem o perfil.
-    getProfile();
+
     UIkit.modal("#my-id").show();
 
   })
@@ -50,7 +49,9 @@ function getProfile(){
 	    contentType: 'application/json; charset=utf-8',
 	    success: function(response){
         console.log(response);
-        console.log(response.user.posts.author);
+        console.log("CHAMOU POPULATE");
+        populatePostCard(response.user.posts);
+        //console.log(response.user.posts.author);
 
         $("#username").val(response.user.username);
         $("#first_name").val(response.user.first_name);
@@ -90,4 +91,35 @@ function updatePassword(){
 
     e.preventDefault();
   });
+}
+
+function populatePostCard(data){
+    $.each(data, function(key, value){
+        let card = createPostCard(value.body, value.title, value.post_date);
+        $("#posts").append(card);
+    });
+}
+
+function createPostCard(body, title, post_date){
+    var divCard = document.createElement("div");
+    var divSegment = document.createElement("div");
+    var divContent = document.createElement("div");
+    var imgPost = document.createElement("img");
+    var h3Title = document.createElement("h3");
+
+    $(divCard).attr("class", "card");
+    $(divSegment).attr("class", "ui left piled segment");
+    $(imgPost).attr("class", "ui tiny left floated image");
+    $(h3Title).attr("class", "ui header text-center");
+    $(divContent).attr("class", "floated right text-center description");
+    $(imgPost).attr("src", "../static/img/noavatar.png");
+
+    $(h3Title).html(title);
+    $(divContent).html(body);
+
+    divCard.appendChild(divSegment);
+    divSegment.appendChild(imgPost);
+    divSegment.appendChild(h3Title);
+    divSegment.appendChild(divContent);
+    return divCard;
 }
